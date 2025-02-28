fc5d9fcab7cca187180f2ff71d0d7c3f2fbdedf7
Haru <carpe.tu@icloud.com>
2025-02-28 12:44:28 +0800

    feat(modern-cnn): add DenseNet implementation.

---
+++ modern-cnn/DenseNet/model_dense.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn as nn
+
+from torch.nn import functional as F
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+model_path = "../../checkpoints/densenet/model.pth"
+
+def init_cnn(module):
+    """Initial weights for CNNs."""
+
+    if type(module) == nn.Linear or type(module) == nn.Conv2d:
+        nn.init.xavier_normal_(module.weight)
+
+
+def conv_block(num_channels):
+    return nn.Sequential(
+        nn.LazyBatchNorm2d(), nn.ReLU(),
+        nn.LazyConv2d(num_channels, kernel_size=3, padding=1))
+
+def transition_block(num_channels):
+    return nn.Sequential(
+        nn.LazyBatchNorm2d(), nn.ReLU(),
+        nn.LazyConv2d(num_channels, kernel_size=1),
+        nn.AvgPool2d(kernel_size=2, stride=2))
+
+
+class DenseBlock(nn.Module):
+    def __init__(self, num_convs, num_channels):
+        super(DenseBlock, self).__init__()
+        layer = []
+        for _ in range(num_convs):
+            layer.append(conv_block(num_channels))
+        self.net = nn.Sequential(*layer)
+
+    def forward(self, X):
+        for blk in self.net:
+            Y = blk(X)
+            X = torch.cat((X, Y), dim=1)
+        return X
+
+class DenseNet(nn.Module):
+    def __init__(self, num_channels=64, growth_rate=32,
+                 arch=(4, 4, 4, 4), num_classes=10):
+        super(DenseNet, self).__init__()
+        self.net = nn.Sequential(self.b1())
+        for i, num_convs in enumerate(arch):
+            self.net.add_module(f'dense_blk{i+1}', DenseBlock(num_convs,
+                                                              growth_rate))
+            # The number of output channels in the previous dense block
+            num_channels += num_convs * growth_rate
+            # A transition layer that halves the number of channels is added
+            # between the dense blocks
+            if i != len(arch) - 1:
+                num_channels //= 2
+                self.net.add_module(f'tran_blk{i+1}', transition_block(
+                    num_channels))
+        self.net.add_module('last', nn.Sequential(
+            nn.LazyBatchNorm2d(), nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1)), nn.Flatten(),
+            nn.LazyLinear(num_classes)))
+
+    def b1(self):
+        return nn.Sequential(
+            nn.LazyConv2d(64, kernel_size=7, stride=2, padding=3),
+            nn.LazyBatchNorm2d(), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+
+    def layer_summary(self, X_shape):
+        X = torch.randn(*X_shape)
+        for layer in self.net:
+            X = layer(X)
+            print(layer.__class__.__name__, 'output shape:\t', X.shape)
+
+    def apply_init(self, input, init=None):
+        """initial function."""
+
+        self.forward(input)
+        if init is not None:
+            self.net.apply(init)
+
+    def forward(self, X):
+        return self.net(X)
+
+
+if __name__ == '__main__':
+    model = DenseNet().eval()
+    #INFO: BN Layer expect more than 1 value per channel when training
+    model.layer_summary((1, 1, 224, 224))
+    print("\n", model)
---
+++ modern-cnn/DenseNet/pred.py
@@ -0,0 +1,36 @@
+import torch
+from model_dense import DenseNet, device, model_path
+from dataset import test_data
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+def predict():
+
+    num_classes = 10
+
+    model = DenseNet(num_classes).to(device)
+    model.load_state_dict(torch.load(model_path, weights_only=True))
+
+    model.eval()
+    x, y = test_data[0][0], test_data[0][1]
+    print(x.shape, type(y))
+    with torch.no_grad():
+        x = x.unsqueeze(0).to(device) # unsqueeze to (batch, channels, height, width)
+        # x = x.to(device)
+        pred = model(x)
+        predicted, actual = classes[pred[0].argmax(0)], classes[y]
+        print(f'Predicted: "{predicted}", Actual: "{actual}"')
+
+if __name__ == '__main__':
+    predict()
